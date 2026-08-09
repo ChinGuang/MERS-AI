4602a74a2502a62cfc3c71027354ff61a1b0fb6d
Chin Guang <ganchinguang0622@gmail.com>
2026-08-09 10:33:50 +0800

    chore: remove unused modules and RabbitMQ integration, update .env and refactor TTS config

--- backend/livekit_agent/tts.py
+++ backend/livekit_agent/tts.py
@@ -3,32 +3,34 @@ from functools import lru_cache
 from livekit.agents.tts import tts
 from livekit.plugins import cartesia, elevenlabs
 
-from environment import CARTESIA_API_KEY, TTS_PLUGIN_USED, ELEVENLABS_API_KEY
+from environment import CARTESIA_API_KEY, TTS_PLUGIN_USED, ELEVENLABS_API_KEY, ELEVENLABS_VOICE_ID, ELEVENLABS_MODEL, \
+    CARTESIA_MODEL, CARTESIA_VOICE_ID
+
 
 @lru_cache()
 def get_tts() -> tts.TTS:
     tts_used: tts.TTS | None = None
     match TTS_PLUGIN_USED.upper():
         case "ELEVENLABS":
-            if ELEVENLABS_API_KEY:
+            if ELEVENLABS_API_KEY and ELEVENLABS_MODEL and ELEVENLABS_VOICE_ID:
                 tts_used = elevenlabs.TTS(
-                    model="eleven_multilingual_v2",
-                    voice_id="k0ZTnyOK89zNStW2yGnv",
+                    model=ELEVENLABS_MODEL,
+                    voice_id=ELEVENLABS_VOICE_ID,
                     api_key=ELEVENLABS_API_KEY,
                 )
             else:
-                print("ELEVENLABS_API_KEY is not set, please set it in environment variable")
+                print("ELEVENLABS_API_KEY, ELEVENLABS_MODEL, or ELEVENLABS_VOICE_ID is not set, please set it in environment variable")
         case "CARTESIA", _:
             if TTS_PLUGIN_USED.upper() != "CARTESIA":
                 print("TTS_PLUGIN_USED is not set, using default TTS")
-            if CARTESIA_API_KEY:
+            if CARTESIA_API_KEY and CARTESIA_VOICE_ID and CARTESIA_MODEL:
                 tts_used = cartesia.TTS(
-                    model="sonic-3",
-                    voice="db6b0ed5-d5d3-463d-ae85-518a07d3c2b4",
+                    model=CARTESIA_MODEL,
+                    voice=CARTESIA_VOICE_ID,
                     api_key=CARTESIA_API_KEY
                 )
             else:
-                print("CARTESIA_API_KEY is not set, please set it in environment variable")
+                print("CARTESIA_API_KEY, CARTESIA_MODEL, or CARTESIA_VOICE_ID is not set, please set it in environment variable")
     if tts_used is None:
         print("TTS is not set, please set it in environment variable")
         raise ValueError("TTS is not set, please set it in environment variable")
